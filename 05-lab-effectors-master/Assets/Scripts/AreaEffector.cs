013b2e9b4a466d1977d66109d01a485ba1d54db2
jorge97710 <jazmitia_10@hotmail.com>
2018-02-17 02:14:41 -0600

    lab 5 terminado, trabajando en extra

---
+++ 05-lab-effectors-master/Assets/Scripts/AreaEffector.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaEffector : MonoBehaviour {
+
+	float time =0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		time += Time.deltaTime;
+		
+		if (time > 5){
+			
+		if (time > 10){
+			gameObject.SetActive(true);	
+
+			
+		}else{
+			
+			gameObject.SetActive(false);	
+			
+		}
+		
+		}
+			//	gameObject.SetActive(true);	
+			
+
+		
+		
+				
+			
+		
+	}
+}
